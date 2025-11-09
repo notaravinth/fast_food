779e81c3e2433bdff1fa8777fe080fcd3789a47f
notaravinth <aravinthravichandran2004@gmail.com>
2025-11-09 18:03:26 +0530

    fonts

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
       <View className="flex-1 items-center justify-center bg-white">
-          <Text className="text-xl font-bold text-red-500">
-              Welcome to Nativewind!
+          <Text className="text-5xl text-center font-bold text-primary font-quicksand-bold">
+              Welcome to My React Native App!
           </Text>
       </View>
   );
